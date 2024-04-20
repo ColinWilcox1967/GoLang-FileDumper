37cebd533ba3bf61ffa135b647781453ca39fc58
Colin Wilcox <colinwilcox1967@gmail.com>
2024-04-20 22:14:14 +0100

    add support for binary display

--- binary/binary.go
+++ binary/binary.go
@@ -1,6 +1,6 @@
-package hex
+package binary
 
-import (
+import(
 	"fmt"
 )
 
@@ -9,29 +9,27 @@ func displayableCharacter (ch byte) bool {
 }
 
 func Dump(data []byte, displayWidth int) {
-	
 	for offset := 0; offset <= len(data); offset += displayWidth {
 		str := fmt.Sprintf ("%08d:", offset)
 		for rowIndex := 0; rowIndex < displayWidth; rowIndex++ {
 			if offset + rowIndex < len(data) {
-				str += fmt.Sprintf("%02X ", data[offset + rowIndex])
+				str += fmt.Sprintf("%08b ", data[offset + rowIndex])
 			} else {
-				str += fmt.Sprintf("%02X ",0)
+				str += fmt.Sprintf("%08b ",0)
 			}
 		}
 		str += "  "
 		for rowIndex := 0; rowIndex < displayWidth; rowIndex++ {
 			if offset + rowIndex < len(data) {
 				if displayableCharacter(data[offset+rowIndex]) {
-					str += fmt.Sprintf("%c", data[offset + rowIndex])
+					str += fmt.Sprintf("%08b", data[offset + rowIndex])
 				} else {
 					str += "."
 				}
 			} else {
-				str += fmt.Sprintf("%c", 0)
+				str += fmt.Sprintf("%08b", 0)
 			}
 		}
 		fmt.Println (str)		
 	}
 }
-
--- main.go
+++ main.go
@@ -5,31 +5,54 @@ import (
 	"fmt"
 
 	hex "./hex"
+	binary "./binary"
+	
 	fileutils "./fileutils"
 )
 
 const (
 	hexdump_creation_date = "2024"
-	hexdump_version = "0.1"
+	hexdump_version = "0.2"
 
 	default_silent_mode = false
 	default_bytes_per_row = 8
+
+	// data dump modes
+	dump_as_hex = 0
+	dump_as_binary = 1
 )
 
 var (
 	silentMode bool = default_silent_mode
 	bytesPerRow int = default_bytes_per_row
+	dumpMode int = dump_as_hex
 )
 
 func showBanner() {
-	fmt.Printf ("HexDump Utility %s\n", hexdump_version)
+	fmt.Printf ("FileDumper Utility %s\n", hexdump_version)
 	fmt.Printf ("(c) Colin Wilcox %s\n\n", hexdump_creation_date)
 }
 
 func parseArguments() {
+
+	var hexMode, binaryMode bool
+
 	flag.BoolVar(&silentMode, "silent", default_silent_mode, "Utility operating mode.")
 	flag.IntVar(&bytesPerRow, "width", default_bytes_per_row, "Number of bytes to display per row.")
+	flag.BoolVar(&hexMode, "hex", true, "Dump file contenns in hexadecimal.")
+	flag.BoolVar(&binaryMode, "binary", false, "Dump file contents in binary.")
 	flag.Parse()
+
+	// dump as specified with default as hex
+	if hexMode && binaryMode {
+		dumpMode = dump_as_hex
+	} else {
+		if hexMode {
+			dumpMode = dump_as_hex
+		} else if binaryMode {
+			dumpMode = dump_as_binary
+		} 
+	}
 
 	if bytesPerRow <= 0 {
 		bytesPerRow = default_bytes_per_row
@@ -45,7 +68,12 @@ func main() {
 
 	data, err := fileutils.ReadFile("test.txt")
 	if err == nil {
-		hex.DumpAsHex(data, bytesPerRow)
+		switch(dumpMode) {
+			case dump_as_hex: hex.Dump(data, bytesPerRow)
+			break;
+			case dump_as_binary: binary.Dump(data, bytesPerRow)
+			break;
+		}
 	} else {
 		fmt.Println(err)
 	}
